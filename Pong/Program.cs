4b4ab9bf4ad40e52fe0f2f8c179188cfbb6dbe84
jonhartm <jonhartm@umich.edu>
2018-03-03 14:26:30 -0500

    added a second box (4 triangles)

--- Pong/Program.cs
+++ Pong/Program.cs
@@ -8,7 +8,8 @@
 
     class Program
     {
-        private static GLObject triangle;
+        private static GLObject triangle1;
+        private static GLObject triangle2;
 
         static void Main(string[] args)
         {
@@ -23,29 +24,51 @@
 
         private static void Win_Load()
         {
-            Vertex[] vertices =
+            // Triangle 1
+            Vertex[] tri1_vertices =
             {
-                new Vertex(new Vec3(0.5f,  0.5f, 0.0f)),
-                new Vertex(new Vec3(0.5f, -0.5f, 0.0f)),
-                new Vertex(new Vec3(-0.5f, -0.5f, 0.0f)),
-                new Vertex(new Vec3(-0.5f,  0.5f, 0.0f))
+                new Vertex(new Vec3(0.25f,  0.25f, 0.0f)),
+                new Vertex(new Vec3(0.25f, -0.25f, 0.0f)),
+                new Vertex(new Vec3(-0.25f, -0.25f, 0.0f)),
+                new Vertex(new Vec3(-0.25f,  0.25f, 0.0f))
             };
 
-            short[] indices =
+            short[] tri1_indices =
             {
                 0, 1, 3,  // first Triangle
                 1, 2, 3   // second Triangle
             };
 
-            Shader vertexShader = Shader.LoadVertexShader(@"Shader\Source\helloTriangle.vert.glsl");
-            Shader fragmentShader = Shader.LoadFragmentShader(@"Shader\Source\helloTriangle.frag.glsl");
+            Shader tri1_vertexShader = Shader.LoadVertexShader(@"Shader\Source\helloTriangle.vert.glsl");
+            Shader tri1_fragmentShader = Shader.LoadFragmentShader(@"Shader\Source\helloTriangle.frag.glsl");
 
-            triangle = new GLObject(vertices, indices, new List<Shader>() { vertexShader, fragmentShader });
+            triangle1 = new GLObject(tri1_vertices, tri1_indices, new List<Shader>() { tri1_vertexShader, tri1_fragmentShader });
+
+            // Triangle 2
+            Vertex[] tri2_vertices =
+            {
+                new Vertex(new Vec3(0.75f,  0.75f, 0.0f)),
+                new Vertex(new Vec3(0.75f, 0.25f, 0.0f)),
+                new Vertex(new Vec3(0.25f, 0.25f, 0.0f)),
+                new Vertex(new Vec3(0.25f,  0.75f, 0.0f))
+            };
+
+            short[] tri2_indices =
+            {
+                0, 1, 3,  // first Triangle
+                1, 2, 3   // second Triangle
+            };
+
+            Shader tri2_vertexShader = Shader.LoadVertexShader(@"Shader\Source\helloTriangle.vert.glsl");
+            Shader tri2_fragmentShader = Shader.LoadFragmentShader(@"Shader\Source\helloTriangle.frag.glsl");
+
+            triangle2 = new GLObject(tri2_vertices, tri2_indices, new List<Shader>() { tri2_vertexShader, tri2_fragmentShader });
         }
 
         private static void Win_Render()
         {
-            triangle.Render();
+            triangle1.Render();
+            triangle2.Render();
         }
     }
 }
